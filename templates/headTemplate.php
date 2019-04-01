05ca1a906409dd26f2a70925745959b1b8722d9f
Simon Dromer <simon.dromer@etu.imt-lille-douai.fr>
2019-04-01 14:21:11 +0200

    création de la template quiz

--- templates/headTemplate.php
+++ templates/headTemplate.php
@@ -3,12 +3,15 @@
 		<head> 
 			<meta charset="utf-8"> 
 			<title>Site web en construction</title> 
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">  
-		   
-<link rel="stylesheet" href="css/style0.css" /> 
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
+		<link rel="stylesheet" href="css/style0.css" /> 
 		<link rel="stylesheet" href="css/styleHome.css" /> 
 		<link rel="stylesheet" href="css/styleInscription.css" /> 
 		<link rel="stylesheet" href="css/styleProfil.css" /> 
-		<link rel="stylesheet" href="css/styleMerci.css" /> 		</head> 
+		<link rel="stylesheet" href="css/styleMerci.css" /> 
+
+
+		
+		</head> 
 		<body> 
 			<div id="page">
