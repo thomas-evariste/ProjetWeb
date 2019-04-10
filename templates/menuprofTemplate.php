17b467ad1b2151e38df4ee2531a5845d490ce943
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-10 14:36:28 +0200

    menu user + menu prof

--- templates/menuprofTemplate.php
+++ templates/menuprofTemplate.php
@@ -1,6 +1,4 @@
-<?php 
-		
-		echo "
+<!--
 		<nav>
 		
 			<ul>
@@ -11,5 +9,43 @@
 				
 			</ul>
 			
-		</nav>";
-?>
+		</nav>-->
+
+
+
+
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?action=home&controller=prof">
+          <i class="fa fa-home"></i>
+          Home
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=profil&controller=prof">
+          <i class="fa fa-user"></i>
+          Profil
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=deconnexion&controller=prof">
+          <i class="fa fa-sign-out-alt"></i>
+          Deconecter
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=creerQuestion&controller=prof">
+          <i class="fa fa-plus-circle"></i>
+          Créer question
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- templates/menuuserTemplate.php
+++ templates/menuuserTemplate.php
@@ -1,14 +1,31 @@
-<?php 
-		
-		echo "
-		<nav>
-		
-			<ul>
-				<li><a href=\"index.php?action=home&controller=user\">Home</a></li>
-				<li><a href=\"index.php?action=profil&controller=user\">Profil</a></li>
-				<li><a href=\"index.php?action=deconnexion&controller=user\">Deconnecter</a></li>
-				
-			</ul>
-			
-		</nav>";
-?>
+
+
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?action=home&controller=user">
+          <i class="fa fa-home"></i>
+          Home
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=profil&controller=user">
+          <i class="fa fa-user"></i>
+          Profil
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=deconnexion&controller=user">
+          <i class="fa fa-sign-out-alt"></i>
+          Deconecter
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
