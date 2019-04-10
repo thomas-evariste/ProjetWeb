5b11e36211e6589ae3ffecd648073ae9d8f11760
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-10 14:06:45 +0200

    Menu hors connexion + diff inscription

--- templates/menuTemplate.php
+++ templates/menuTemplate.php
@@ -1,5 +1,4 @@
-<?php 
-		echo "
+<!--
 		<nav>
 	
 			<ul>
@@ -11,5 +10,39 @@
 		
 		</nav>";
 	
+-->
 
-?>
+
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?action=home">
+          <i class="fa fa-home"></i>
+          Home
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=login">
+          <i class="fa fa-sign-in-alt"></i>
+          Login
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-user-plus"></i>
+          Inscription
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="index.php?action=inscription">Eleve</a>
+          <a class="dropdown-item" href="index.php?action=inscriptionprof">Prof</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
