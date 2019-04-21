1bf61ff8799f7668f2fc0f407eff86c525048b6e
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-21 17:21:14 +0200

    abération bdd

--- sql/donnees.sql
+++ sql/donnees.sql
@@ -30,13 +30,12 @@ SET FOREIGN_KEY_CHECKS=0;
 --
 
 INSERT INTO `ENSEIGNANT` (`ID_USER`, `LOGIN`, `PASSWORD`, `INTERNE`, `DESCRIPTION`, `NOM`, `PRENOM`, `MAIL`) VALUES
-(4, '', '', 0, 'Intervenant de MIBI', 'CORDONNIER', 'Jean-Loup', 'jl@c.com'),
-(5, '', '', 1, 'Professor of opérational research', 'LUJAK', 'Marin', 'bidule@mail.com'),
-(6, '', '', 0, 'Professeur de mathématiques', 'ATALI', 'Jack', 'test@mail.com'),
-(7, '', '', 1, 'Doctorant', 'NEWTON', 'Isaac', 'op@mail.com'),
-(8, '', '', 1, 'Professeur de mathématiques', 'COQUEREL', 'Quentin', 'qc@mail.com'),
-(1, '', '', 1, 'Professeur de mathématiques', 'COQUEREL', 'Quentin', 'qc@mail.com'),
-(2, 'simon', 'azerty', 1, 'Super Prof', 'DROMER', 'Simon', 'simon.dromer@etu.imt-lille-douai.fr');
+(4, 'a', 'a', 0, 'Intervenant de MIBI', 'CORDONNIER', 'Jean-Loup', 'jl@c.com'),
+(5, 'b', 'b', 1, 'Professor of opérational research', 'LUJAK', 'Marin', 'bidule@mail.com'),
+(6, 'c', 'c', 0, 'Professeur de mathématiques', 'ATALI', 'Jack', 'test@mail.com'),
+(7, 'd', 'd', 1, 'Doctorant', 'NEWTON', 'Isaac', 'op@mail.com'),
+(8, 'e', 'e', 1, 'Professeur de mathématiques', 'COQUEREL', 'Quentin', 'qc@mail.com'),
+(1, 'f', 'f', 1, 'Professeur de mathématiques', 'COQUEREL', 'Quentin', 'qc@mail.com'),
 
 
 --
