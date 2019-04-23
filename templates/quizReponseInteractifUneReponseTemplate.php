965fb70ce466182b7b69869b3bc47d5643c6c20a
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-23 06:42:17 +0200

    réparer qcu

--- templates/quizReponseInteractifUneReponseTemplate.php
+++ templates/quizReponseInteractifUneReponseTemplate.php
@@ -3,7 +3,7 @@
                     
                         <div class="col-md-4">
                             <div class="frb frb-success margin-bottom-none">
-    						<input type="<?php echo $type ?>" id="<?php echo $type ?>-button-<?php echo $numero+1 ?>-<?php echo $numero_reponse+1 ?>" name="<?php echo $type ?>-button-<?php echo $numero+1 ?>-<?php echo $numero_reponse+1 ?>" value="<?php echo $reponse['id_proposition'] ?>">
+    						<input type="<?php echo $type ?>" id="<?php echo $type ?>-button-<?php echo $numero+1 ?>-<?php echo $numero_reponse+1 ?>" name="<?php echo $type ?>-button-<?php echo $numero+1 ?><?php if($type=='checkbox') {echo '-' ;echo $numero_reponse+1;} ?>" value="<?php echo $reponse['id_proposition'] ?>">
     						<label for="<?php echo $type ?>-button-<?php echo $numero+1 ?>-<?php echo $numero_reponse+1 ?>">
     							<span class="frb-title"><?php echo $reponse['intitule_proposition'] ?></span>
     						</label>
