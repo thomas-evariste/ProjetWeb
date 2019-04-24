f51af30869c613cfbfd899e6bb8fdeb38005e150
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-24 20:52:07 +0200

    avancement

--- templates/resultatQuestionnairesProfTemplate.php
+++ templates/resultatQuestionnairesProfTemplate.php
@@ -15,6 +15,9 @@
                 echo "<div class=\"etat\">Etat: ". $questionnaire['etat'] ." </div>";
                 echo "<div class=\"createur\">Createur: ". $questionnaire['createur'] ." </div>";
                 echo "<div class=\"note\">Note: ". $questionnaire['note']." sur ". $questionnaire['noteMax'] ." </div>";
+				if($questionnaire['corrige']==1){
+					echo "<div>Ce questionnaire n'a pas encore été totalement corrigé </div>";
+				}
                 echo "<div class=\"boutons\">
 				<form action=\"index.php?action=classementQuiz&controller=prof\" method=\"POST\"><input type='hidden' name='questionnaireId' value='".$questionnaire['id']."'><input type='submit' value='Classement'></form>
 				";
