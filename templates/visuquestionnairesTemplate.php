74e898c69054d34a37485dd7dffef74ba02a51bf
Thomas Guyot <thomas.Evariste.guyot@gmail.com>
2019-04-24 05:24:23 +0200

    afficher les date au format fr sur le site

--- templates/visuquestionnairesTemplate.php
+++ templates/visuquestionnairesTemplate.php
@@ -20,11 +20,23 @@
         <?php 
 
             foreach($questionnaires as $questionnaire){
+				if($questionnaire['dateOuverture']!=""){
+					$dateStringO=substr($questionnaire['dateOuverture'],-2,2).'-'.substr($questionnaire['dateOuverture'],5,2).'-'.substr($questionnaire['dateOuverture'],0,4);
+				}
+				else{
+					$dateStringO = "";
+				}
+				if($questionnaire['dateFermeture']!=""){
+					$dateStringF=substr($questionnaire['dateFermeture'],-2,2).'-'.substr($questionnaire['dateFermeture'],5,2).'-'.substr($questionnaire['dateFermeture'],0,4);
+				}
+				else{
+					$dateStringF = "";
+				}
                 echo "<tr>   
                 <th>" . $questionnaire['titre'] ." </th>
                 <th>" . $questionnaire['description']."</th>
-                <th>" . $questionnaire['dateOuverture'] ."</th>
-                <th>" . $questionnaire['dateFermeture'] ."</th>
+                <th>" . $dateStringO ."</th>
+                <th>" . $dateStringF ."</th>
                 <th>" . $questionnaire['connexionRequise'] ."</th>
                 <th>" . $questionnaire['etat'] ."</th>
                 <th>" . $questionnaire['url'] ."</th>
